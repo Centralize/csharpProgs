47014e6096aeb4cde1f5d04b881bf68c4176f499
Mikkel Kaas <mikkel.kaas.64@gmail.com>
2020-10-27 10:08:41 +0100

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Counter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double tal = 1;
+            
+            while (true)
+            {
+                Console.WriteLine(Convert.ToString(tal));
+                tal++;
+            }
+        }
+    }
+}
